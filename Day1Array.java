d4f65741d3f2853871e1f0da40036d1e34e91eea
William Dasari <66524338+William-Dasari@users.noreply.github.com>
2022-08-04 22:36:51 +0530

    Add files via upload
    sorting an array upto 50 elements

--- Day1Array.java
+++ Day1Array.java
@@ -10,8 +10,27 @@ public class Day1Array {
         {
           num[i]=scanner.nextInt();
         }
-        for(int j=0;j<num.length;j++){
-            System.out.print(num[j]+",");
+        for(int j=0;j<num.length;j++)
+        {
+            if(num[j]<=10) {
+                System.out.print(num[j] + ",");
+            }
+            else if(num[j]>10&&num[j]<=20) {
+                System.out.println(num[j] + ",");
+            }
+            else if(num[j]>20&&num[j]<=30) {
+                System.out.println(num[j] + ",");
+            }
+            else if(num[j]>30&&num[j]<=40) {
+                System.out.println(num[j] + ",");
+            }
+            else if(num[j]>40&&num[j]<=50) {
+                System.out.println(num[j] + ",");
+            }
+            else {
+                System.out.println("beyond the range");
+            }
+
         }
     }
 }
